ba5ab08a240208659b916e79dd7a4121075b9be2
Gerald-ux-ux <11geraldmoney@gmail.com>
2024-08-27 20:33:17 +0300

    feat: configuring the port at runtime

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
-
+import { config } from 'dotenv';
+config(); // Load environment variables from .env file
 import { AppModule } from './app.module';
 
-const port = process.env.PORT || 3000;
-console.log(
-  `Launching NestJS app on port ${port}, URL: http://0.0.0.0:${port}`,
-);
 async function bootstrap() {
+  const port = process.env.PORT || 3000;
+  console.log(
+    `Launching NestJS app on port ${port}, URL: http://0.0.0.0:${port}`,
+  );
   const app = await NestFactory.create(AppModule);
   await app.listen(port);
 }

8229daa9148471376f4b451d1fda02a0f87911d7
Gerald-ux-ux <11geraldmoney@gmail.com>
2024-09-17 22:20:14 +0300

    build: changing the api key

--- src/cords/cords.service.ts
+++ src/cords/cords.service.ts
@@ -1,12 +1,17 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { lastValueFrom, map } from 'rxjs';
 
 @Injectable()
 export class CordsService {
-  constructor(private readonly httpService: HttpService) {}
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly configService: ConfigService,
+  ) {}
   async getCityCords(city: string) {
-    const api = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=8cbb90a03f4439fa1655c1e779d5ca92&state`;
+    const apiKey = this.configService.get<string>('API_KEY');
+    const api = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}&state`;
 
     try {
       // Getting the data from the response and converting it to a readable value
